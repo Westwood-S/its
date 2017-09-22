82b0f4b5a7dec9669dcc7b87aedf70359debe8bb
unknown <yzwang@outlook.com>
2017-09-22 13:15:06 +0800

    优化

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ int_sd_updated_time = ''
 dom_sd_updated_time = ''
 news_updated_time = ''
 config = {}
+import_data = {}
 
 def send_email(to: str, subject: str, text: str=None):
     _user = "375002410@qq.com"
@@ -46,38 +47,69 @@ def int_sd_updated(s:requests.session):
         return False
     else:
         int_sd_updated_time = latest_time
-        config["int_sd_updated_time"] = latest_time
-        with open("configure.json", "w") as f:
-            json.dump(config, f)
+        import_data["int_sd_updated_time"] = latest_time
+        with open("data.json", "w") as f:
+            json.dump(import_data, f)
+        return True
+
+def dom_sd_updated(s:requests.session):
+    global dom_sd_updated_time
+    global config
+
+    data = s.get(url='http://cn.its.glo-ots.cn/ITS_MSG_Dom_sd.asp')
+    data.encoding = 'gbk'
+    times = re.search(r'(\d)+/(\d+)/(\d+) (\d+):(\d+):(\d+)', data.text)
+    latest_time = times.group(0)
+    if dom_sd_updated_time == latest_time:
+        return False
+    else:
+        dom_sd_updated_time = latest_time
+        import_data["dom_sd_updated_time"] = latest_time
+        with open("data.json", "w") as f:
+            json.dump(import_data, f)
         return True
 
 def main():
-    username = 'ftcl03'
-    password = 'Stl292707'
-    sleep_time = 60
-    email = "375002410@qq.com"
+    print ("开始运行了……")
+    username = ''
+    password = ''
+    sleep_time = 0
+    email = ''
     global config
+    global import_data
     global int_sd_updated_time
     global dom_sd_updated_time
     global news_updated_time
 
-    with open("configure.json", "r") as f:
+    with open("config.json", "r") as f:
         config = json.load(f)
+    with open("data.json", "r") as f:
+        import_data = json.load(f)
     username = config["username"]
     password = config["password"]
     sleep_time = config["time"]
     email = config["email"]
-    int_sd_updated_time = config["int_sd_updated_time"]
-    dom_sd_updated_time = config["dom_sd_updated_time"]
-    news_updated_time = config["news_updated_time"]
+    int_sd_updated_time = import_data["int_sd_updated_time"]
+    dom_sd_updated_time = import_data["dom_sd_updated_time"]
+    news_updated_time = import_data["news_updated_time"]
+    print ("用户名:{}".format(username))
+    print ("密码:{}".format(password))
+    print ("采集周期:{}".format(sleep_time))
+    print ("发送邮箱:{}".format(email))
+    print ("导入海外供求更新时间:{}".format(int_sd_updated_time))
+    print ("导入本地供求更新时间:{}".format(dom_sd_updated_time))
+    print ("导入新闻更新时间:{}".format(news_updated_time))
 
     session = requests.session()
     payload = {'account': username, 'password': password}
     session.post(url='http://cn.its.glo-ots.cn/login.asp', data=payload)
 
     while True:
-        if int_sd_updated (session):
-            send_email(email, "int_sd_updated")
+        if int_sd_updated(session):
+            send_email(email, "海外供求更新")
+        if dom_sd_updated(session):
+            send_email(email, "本地求购更新")
+
         time.sleep(sleep_time)
 
 if __name__ == '__main__':
